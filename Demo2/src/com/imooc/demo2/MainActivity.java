7b864af2579f0c3fa5d6f53b3b41d325e44d79bc
huge-potato <fubin199410@qq.com>
2017-01-16 13:00:02 +0800

    add MultiAutoCompleteTextView多个匹配功能

--- Demo2/src/com/imooc/demo2/MainActivity.java
+++ Demo2/src/com/imooc/demo2/MainActivity.java
@@ -1,5 +1,8 @@
 package com.imooc.demo2;
-
+/*
+ * AutoCompletemTextView与MultiAutoCompleteTextView区别前者只能匹配一个，后者可匹配多个，每个用分隔符
+ * 分开
+ */
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -11,12 +14,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 	private AutoCompleteTextView acTextView;
+	private MultiAutoCompleteTextView macTextView;
 	private String[] res = {"beijing1","beijing2","beijing3","shanghai1","shanghai2","android"};
-    @Override
+   
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -30,6 +36,18 @@ public class MainActivity extends ActionBarActivity {
        //最简单的适配器
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, res);
         acTextView.setAdapter(adapter);
-    }
+        
+        /*MultiAutoCompleteTextView
+         * 第一步：初始化控件
+         * 第二步：需要一个适配器（用这个适配器匹配输入内容）
+         * 第三步，初始化数据源--这数据源去匹配文本框输入的内容
+         * 第四步：将adepter与当前MultiAutoCompleteTextView控件绑定
+         * 第五步：设置分隔符
+         */
+        macTextView = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView1);
+        macTextView.setAdapter(adapter);
+        //设置分隔符“，”
+        macTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+	}
 
 }
